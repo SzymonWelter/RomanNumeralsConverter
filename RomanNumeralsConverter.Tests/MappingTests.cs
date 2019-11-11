1f145f826eb30dd8f92a59fd7982c0af77dea6f1
SzymonWelter <szymon.welter98@gmail.com>
2019-11-11 20:57:07 +0100

    mapping tests

---
+++ RomanNumeralsConverter.Tests/MappingTests.cs
@@ -0,0 +1,38 @@
+﻿using RomanNumeralsConverter.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RomanNumeralsConverter.Tests
+{
+    public class MappingTests
+    {
+        private readonly IMapService<int,string> mapService;
+
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(13, "XIII")]
+        [InlineData(39, "XXXIX")]
+        [InlineData(49, "XLIX")]
+        [InlineData(89, "LXXXIX")]
+        [InlineData(399, "CCCXCIX")]
+        [InlineData(499, "CDXCIX")]
+        [InlineData(894, "DCCCXCIV")]
+        [InlineData(999, "MCXCIX")]
+        [InlineData(3999, "MMMCMXCIX")]
+        public void WhenNumberIsZero_ShouldReturnEmptyString(int arg, string expected)
+        {
+            var actual = mapService.Map(arg);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ RomanNumeralsConverter/Mapping/IMapService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumeralsConverter.Mapping
+{
+    interface IMapService<TFrom, TTo>
+    {
+        TTo Map(TFrom source);
+    }
+}
